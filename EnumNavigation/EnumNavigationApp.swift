39e62e5831dadeed9e9daa79e2c7afa5b3f7b96e
Ramill <ramill_ibragimov@icloud.com>
2022-12-18 18:34:28 +0200

    Initial Commit

---
+++ EnumNavigation/EnumNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnumNavigationApp.swift
+//  EnumNavigation
+//
+//  Created by Ramill Ibragimov on 18.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct EnumNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
